be168003f505d175432b8050c310d8c840dd7469
chantatha.p <chantatha.p@entronica.co.th>
2024-07-15 21:49:29 +0700

    finished mock

--- app.ts
+++ app.ts
@@ -1,16 +1,10 @@
 import express from 'express';
-import { json } from 'body-parser';
 import { nmgwRouter } from './src/nmgw.app';
-
 const app = express();
 const port = 3000;
+app.use(express.json({}));
+app.use('/api/v1/nmgw/', nmgwRouter);
 
-app.use(json());
-
-// Use the router from nmgw.app.ts
-app.use('/api/nmgw/', nmgwRouter);
-
-// Start the server
 app.listen(port, () => {
   console.log(`Mock API running at http://localhost:${port}`);
 });
